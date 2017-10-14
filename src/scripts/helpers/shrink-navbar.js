5f74ea48e01083d41857770e896236fbc01cc63d
Tanju Yıldız <ben@tanjuyildiz.com>
2017-10-14 17:23:24 +0300

    Header alanı düzenlendi.

--- src/scripts/helpers/shrink-navbar.js
+++ src/scripts/helpers/shrink-navbar.js
@@ -2,14 +2,27 @@ jQuery(document).ready(function($){
   /**
    * Shrink Navbar
    */
+  var $body = $('body');
   var $window = $(window);
-  var navbar = $('#header');
+  var windowWidth = $window.width();
+  var headerContainer = $('#header');
+  var getHeaderHeight = headerContainer.outerHeight();
+  var lastScrollPosition = 0;
 
-  $window.scroll(function() {
-    if ($window.scrollTop() > 100) {
-      navbar.addClass('fixed-nav');
-    } else {
-      navbar.removeClass('fixed-nav');
-    }
-  });
+  if ( windowWidth > 768 ) {
+    $window.scroll(function() {
+      var currentScrollPosition = $window.scrollTop();
+      if ( $(window).scrollTop() > 2 * getHeaderHeight ) {
+        $body.addClass('scroll-active').css('padding-top', getHeaderHeight);
+        headerContainer.css('top', 0);
+        if ( currentScrollPosition < lastScrollPosition ) {
+          headerContainer.css('top', '-' + getHeaderHeight + 'px');
+        }
+        lastScrollPosition = currentScrollPosition;
+      } else {
+        $body.removeClass('scroll-active').css('padding-top', 0);
+        headerContainer.css('top', 0);
+      }
+    });
+  }
 });
