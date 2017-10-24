178e6157c971bbea9a0a4bbedf0ad9492e799272
Tanju Yıldız <ben@tanjuyildiz.com>
2017-10-24 10:18:49 +0300

    Gönüllü Ol sayfası oluşturuldu.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,4 +11,22 @@ jQuery(document).ready(function($){
         + '<div class="count-item"><span class="count-time">%S</span><span class="count-label">saniye</span></div>'));
     });
   });
-})
+
+  //
+  // Yüklenen Resmi Önizleme Yapma
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('#blah').attr('src', e.target.result);
+        $('#blah').hide();
+        $('#blah').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#imgInp").change(function() {
+    readURL(this);
+  });
+});
